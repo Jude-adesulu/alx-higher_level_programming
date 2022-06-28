8d87e12af7347e9d4649343bc39cd978d97a4bd5
jude-adesulu <judeadesuluflyer6@gmail.com>
2022-06-28 21:37:10 +0000

    Print alphabets in lowercase

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     last_digit = number % 10
 else:
-    ((number * -1) % 10) * -1
+    last_digit = ((number * -1) % 10) * -1
 message = f"Last digit of {number} is {last_digit} and is"
 
 if last_digit == 0:
