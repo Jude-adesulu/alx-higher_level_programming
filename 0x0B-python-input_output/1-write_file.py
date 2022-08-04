5ab38e8c79119c309efcc554b602705c85a0aa34
Jude Adesulu <judeadesuluflyer6@gmail.com>
2022-08-04 04:24:36 +0100

    Update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-'''task 1 module'''
+"""
+function that writes a string to a text file (UTF8) and returns the number of characters written
+"""
 
 
 def write_file(filename="", text=""):
-    '''writes a string to  file overwrites if exist'''
-    with open(filename, mode='w', encoding='utf-8') as f:
-        len = f.write(text)
-
-    return 
+    """ module write_file
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
